4c3c505d1e9fbc6ca0d5901487194cd9a320cb1a
Mathew057 <mathew0057@gmail.com>
2018-07-30 21:21:13 -0500

    changed init script

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+echo "$USER_NAME $PASSWORD $DEVICE_NAME"
 gdrive_download () {
 	CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
        	wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
@@ -41,17 +42,18 @@ raspi-config nonint do_camera 0
 raspi-config nonint do_vnc 0
 
 mkdir /home/pi/.config/autostart
-cp tracker.desktop /home/pi/.config/autostart
+cp /home/pi/people-tracker/tracker.desktop /home/pi/.config/autostart
+cp /home/pi/people-tracker/tracker.desktop /home/pi/Desktop
 
-sed -i "s/<USER_NAME>/$USER_NAME/g" keys.txt
-sed -i "s/<PASSWORD>/$PASSWORD/g" keys.txt
-sed -i "s/<DEVICE_NAME>/$DEVICE_NAME/g" keys.txt
+sed -i "s/<USER_NAME>/$USER_NAME/g" /home/pi/people-tracker/keys.txt
+sed -i "s/<PASSWORD>/$PASSWORD/g" /home/pi/people-tracker/keys.txt
+sed -i "s/<DEVICE_NAME>/$DEVICE_NAME/g" /home/pi/people-tracker/keys.txt
 
-echo "$PASSWORD" > passwd.txt
-echo "$PASSWORD" >> passwd.txt
+echo "$PASSWORD" > /home/pi/people-tracker/passwd.txt
+echo "$PASSWORD" >> /home/pi/people-tracker/passwd.txt
 
-weavedinstaller < keys.txt
+weavedinstaller < /home/pi/people-tracker/keys.txt
 echo 'Authentication=VncAuth' /root/.vnc/config.d/vncserver-x11
 
-vncpasswd -service < passwd.txt
-rm passwd.txt
+vncpasswd -service < /home/pi/people-tracker/passwd.txt
+rm /home/pi/people-tracker/passwd.txt
