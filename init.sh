71f28691bef68136e6fae4a20c17255ed0677bba
Mathew057 <mathew0057@gmail.com>
2018-08-30 21:21:32 -0500

    Fixed downloading issue with other regions

--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-gdrive_download () {
-	CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-       	wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
-	rm -rf /tmp/cookies.txt
-}
-
 apt-get update
 apt-get install build-essential cmake pkg-config -y
 apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
@@ -21,10 +15,10 @@ export LIBRARY_PATH=/opt/vc/lib
 
 cd /home/pi/people-tracker
 
-gdrive_download 1SUyGrcwf2PMz9hjLw28FJkju7g09de1P opencv.zip
+python google_drive.py 1SUyGrcwf2PMz9hjLw28FJkju7g09de1P opencv.zip
 unzip opencv.zip
 
-gdrive_download 17O4Z5oe_9k_jwMjI3cmDoKAsHzaVN-Rt raspicam.zip
+python google_drive.py 17O4Z5oe_9k_jwMjI3cmDoKAsHzaVN-Rt raspicam.zip
 unzip raspicam.zip
 
 cd /home/pi/people-tracker/opencv/opencv-3.4.2/build
